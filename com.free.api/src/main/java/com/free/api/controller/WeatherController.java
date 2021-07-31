589d70088ebc99dec1f9de041567ebd31e9a2535
canmengqian <2570389155@qq.com>
2021-07-31 22:59:36 +0800

    swagger

--- com.free.api/src/main/java/com/free/api/controller/WeatherController.java
+++ com.free.api/src/main/java/com/free/api/controller/WeatherController.java
@@ -1,6 +1,15 @@
 package com.free.api.controller;
 
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import com.github.xiaoymin.knife4j.annotations.ApiSupport;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author zhengzz
@@ -10,5 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/7/28
  */
 @RestController
+@RequestMapping("/api")
+@Api(tags = "2.0.3版本-20200312")
+@ApiSupport(author = "xiaoymin@foxmail.com",order = 284)
 public class WeatherController {
+
+    @Resource
+    Environment environment;
+
+    @ApiOperationSupport(author = "xiaoymin@foxmail.com")
+    @ApiOperation(value = "写文档注释我是认真的")
+    @GetMapping("/index")
+    public  String index() {
+        return  environment.getProperty ("server.port");
+    }
 }
